3a0761494613bb4d8955952b94c3c6e70efa0a8c
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-03-07 15:08:14 +0000

    Refactor

--- src/main/kotlin/Domain.kt
+++ src/main/kotlin/Domain.kt
@@ -1,8 +1,4 @@
 import Interfaces.TodoListRepoInterface
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
-import java.io.File
 
 class Domain(val todoListRepo: TodoListRepoInterface) {
 
--- src/main/kotlin/HttpAPI.kt
+++ src/main/kotlin/HttpAPI.kt
@@ -10,15 +10,12 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.http4k.core.*
 
-
 val mapper: ObjectMapper = jacksonObjectMapper() // tool to allow us to convert to and from JSON data
 
 val app: HttpHandler = routes(
     "/todos" bind GET to {request: Request ->
         val todoId: String = request.query("todoId")?: "" // handle errors if id is incorrect
 
-        println(todoId)
-        println("hello test")
         val todoListRepo: TodoListRepoInterface = TodoListRepoJSON()
         val domain = Domain(todoListRepo)
         val todoList: List<TodoItem> = domain.getTodoList(todoId)
@@ -33,39 +30,6 @@ fun main() {
     val server = printingApp.asServer(SunHttp(9000)).start()
 
     println("Server started on " + server.port())
-
-
 }
 
 
-
-//
-//        val taskId: String = request.query("id")
-
-// if no query parameter, return entire list
-//        if (taskId == null) {
-//            Response(OK).body(toDoListAsJsonString)
-// if taskId declared, return task matching the id
-//        } else {
-//            val singleTask: List<TodoItem> = toDoList.filter {
-//                it.id == taskId }
-//                    Response(OK).body(mapper.writeValueAsString(singleTask))
-//                }
-// add an item to task list
-//    "/todo" bind POST to {request: Request ->
-//        val jsonStringNewTodo = request.bodyString()
-//        val newTodo: TodoItem = mapper.readValue(jsonStringNewTodo, TodoItem::class.java) // telling the object mapper to instantiate the ToDoItem class when it parses the jsonString
-//        todoList.add(newTodo)
-//        val updatedListAsJson = mapper.writeValueAsString(todoList)
-//        Response(OK).body("your todo has been added" )
-//    }
-
-
-
-//    "/formats/json/jackson" bind GET to {
-//        Response(OK).with(jacksonMessageLens of JacksonMessage("Barry", "Hello there!"))
-//    },
-//
-//    "/testing/hamkrest" bind GET to {request ->
-//        Response(OK).body("Echo '${request.bodyString()}'")
-//    }
