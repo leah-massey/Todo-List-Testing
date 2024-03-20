21e28d36012539f81a8c0b84eecce4fac4c98535
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-03-20 21:04:30 +0000

    Feat: a list of unaltered todos can be retrieved from the event file repo

--- src/main/kotlin/adapters/TodoListEventFileRepo.kt
+++ src/main/kotlin/adapters/TodoListEventFileRepo.kt
@@ -1,6 +1,8 @@
 package adapters
 
+import domain.models.TodoCreatedEvent
 import domain.models.TodoEvent
+import domain.models.TodoNameUpdatedEvent
 import org.http4k.format.Jackson.mapper
 import ports.TodoListEventRepo
 import java.io.BufferedReader
@@ -17,16 +19,42 @@ class TodoListEventFileRepo(filePath: String): TodoListEventRepo {
         fileWriter.close()
     }
 
+//    override fun getEvents(): List<TodoEvent> {
+//        val eventList = mutableListOf<TodoEvent>()
+//        val fileReader = BufferedReader(FileReader(eventFileRepo))
+//        var line: String?
+//        while (fileReader.readLine().also { line = it } != null) {
+//            val event: TodoEvent = mapper.readValue(line, TodoEvent::class.java) // second param is optional but specifies what I want to serialize my 'line' into
+//            eventList.add(event)
+//        }
+//        fileReader.close()
+//        return eventList
+//    }
+
     override fun getEvents(): List<TodoEvent> {
         val eventList = mutableListOf<TodoEvent>()
         val fileReader = BufferedReader(FileReader(eventFileRepo))
         var line: String?
-        while (fileReader.readLine().also { line = it } != null) { // read each line, assigning each line to it for each loop. 'it' then = an event
-            val event: TodoEvent = mapper.readValue(line, TodoEvent::class.java) // second param is optional but specifies what I want to serialize my 'line' into
-            eventList.add(event)
+
+        while (fileReader.readLine().also { line = it } != null) {  // read each line, assigning each line to it for each loop. 'it' then represents an event
+            val jsonNode = mapper.readTree(line) // parse the JSON string representation of the event into a JsonNode
+            val eventType = jsonNode.get("eventType")?.asText()
+            val event: TodoEvent? = when (eventType) {
+                "TODO_CREATED" -> mapper.readValue(line, TodoCreatedEvent::class.java)
+                "TODO_NAME_UPDATED" -> mapper.readValue(line, TodoNameUpdatedEvent::class.java)
+                // Add more cases
+                else -> null
+            }
+            if (event != null) {
+                eventList.add(event)
+            } else {
+                println("Error: handle")
+            }
         }
         fileReader.close()
+
         return eventList
     }
+
 
 }
--- src/main/kotlin/domain/ReadDomain.kt
+++ src/main/kotlin/domain/ReadDomain.kt
@@ -2,56 +2,49 @@ package domain
 
 import adapters.TodoListEventFileRepo
 import adapters.TodoListFileRepo
-import domain.models.TodoEssentials
-import domain.models.Todo
-import domain.models.TodoEssentialsByStatus
-import domain.models.TodoEvent
+import domain.models.*
 import ports.TodoListEventRepo
 import ports.TodoListRepo
 
-class ReadDomain(val todoListRepo: TodoListRepo, val todoListEventRepo: TodoListEventRepo) {
-
-    fun getTodoListEssentials(todoId: String = ""): List<TodoEssentials> {
-        return getTodoList(todoId).map{todo ->
-            todoEssentials(todo)
-        }
-    }
-
-    fun getTodoListEssentialsByStatus(status: String): List<TodoEssentialsByStatus> {
-        return getTodosByStatus(status).map{todo ->
-            todoEssentialsByStatus(todo)
-        }
-    }
-
-    fun getTodosByStatus(status: String): List<Todo> {
-        val todoList: List<Todo> = getTodoList("")
-        return todoList.filter { todo ->
-            todo.status == status
-        }
-    }
-
-    fun getTodoList(todoId: String): List<Todo>  {
-        val todoList: List<Todo> = todoListRepo.getTodos()
-
-        if (todoId == "") {
-            return todoList
-        } else {
-            return todoList.filter {
-                it.id == todoId }
-        }
-    }
+class ReadDomain( val todoListEventRepo: TodoListEventRepo) {
 
     fun getTodoListEvents(): List<TodoEvent> {
         val todoListEvents: List<TodoEvent> = todoListEventRepo.getEvents()
         return todoListEvents
     }
 
+    fun getTodoListEssentials(todoId: String = ""): List<TodoEssentials> {
+        val todoListEvents: List<TodoEvent> = getTodoListEvents()
+
+        return todoListEvents.mapNotNull { todoEvent ->
+            when (todoEvent) {
+                is TodoCreatedEvent -> {
+                    val todo: Todo = todoEvent.eventDetails
+                    todoEssentials(todo)
+                }
+                else -> null
+            }
+        }
+    }
+
+//    fun getTodosByStatus(status: String): List<Todo> {
+//        val todoList: List<Todo> = getTodoList("")
+//        return todoList.filter { todo ->
+//            todo.status == status
+//        }
+//    }
+
+//    fun getTodoListEssentialsByStatus(status: String): List<TodoEssentialsByStatus> {
+//        return getTodosByStatus(status).map{todo ->
+//            todoEssentialsByStatus(todo)
+//        }
+//    }
+
 
 
     private fun todoEssentialsByStatus(todo: Todo): TodoEssentialsByStatus {
         return TodoEssentialsByStatus(id = todo.id, name = todo.name)
     }
-
 }
 
 // where should this be kept if it is used by both readDomain and writeDomain?
@@ -62,9 +55,9 @@ fun todoEssentials(todo: Todo): TodoEssentials {
 fun main() {
     val todoListRepo: TodoListRepo = TodoListFileRepo("./src/resources/todo_list.json")
     val todoListEventRepo: TodoListEventRepo = TodoListEventFileRepo("./src/resources/todo_list_event_log.ndjson")
-    val readDomain = ReadDomain(todoListRepo, todoListEventRepo)
+    val readDomain = ReadDomain(todoListEventRepo)
     val writeDomain = WriteDomain(todoListRepo, todoListEventRepo, readDomain)
 
-
     println(readDomain.getTodoListEvents())
+    println(readDomain.getTodoListEssentials())
 }
