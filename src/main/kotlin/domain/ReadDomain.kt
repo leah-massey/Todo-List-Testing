d63840f61e243bddfb2bf79c0d536a5b91186408
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-03-25 17:08:09 +0000

    Refactor: tidy up of notes

--- src/main/kotlin/domain/ReadDomain.kt
+++ src/main/kotlin/domain/ReadDomain.kt
@@ -66,61 +66,6 @@ class ReadDomain(val todoListEventRepo: TodoListEventRepo) {
 }
 
 
-// getTodoListEventsById
-
-// refactor and make it neater -
-//fun getTodoListClientView(todoId: String = ""): List<TodoClientView> {
-//    if (todoId == "") {
-//
-//        // fun getAllTodos()
-//        // todoListEventRepo.
-//
-//        return getTodoListEvents().mapNotNull { todoEvent ->
-//            when (todoEvent) {
-//                is TodoCreatedEvent -> {
-//                    val todo: Todo = todoEvent.eventDetails
-//                    todoClientView(todo)
-//                }
-//
-//                else -> null
-//            }
-//        }
-//    } else {
-//
-//        // getTodosById
-//
-//        val eventsForChosenId: List<TodoEvent> = getTodoListEvents().filter { event ->
-//            event.entityId == todoId
-//        }
-//        return eventsForChosenId.mapNotNull { todoEvent ->
-//            when (todoEvent) {
-//                is TodoCreatedEvent -> {
-//                    val todo: Todo = todoEvent.eventDetails
-//                    todoClientView(todo)
-//                }
-//
-//                else -> null
-//            }
-//        }
-//    }
-//
-//
-//}
-
-//    fun getTodosByStatus(status: String): List<Todo> {
-//        val todoList: List<Todo> = getTodoList("")
-//        return todoList.filter { todo ->
-//            todo.status == status
-//        }
-//    }
-
-//    fun getTodoListEssentialsByStatus(status: String): List<TodoEssentialsByStatus> {
-//        return getTodosByStatus(status).map{todo ->
-//            todoEssentialsByStatus(todo)
-//        }
-//    }
-
-
 // where should this be kept if it is used by both readDomain and writeDomain?
 fun todoClientView(todo: Todo): TodoClientView {
     return TodoClientView(id = todo.id, name = todo.name, status = todo.status)
--- src/main/kotlin/domain/WriteDomain.kt
+++ src/main/kotlin/domain/WriteDomain.kt
@@ -1,26 +1,30 @@
 package domain
 
-import adapters.TodoListEventFileRepo
-import adapters.TodoListFileRepo
 import domain.models.*
 import ports.TodoListEventRepo
 import ports.TodoListRepo
 import java.time.LocalDateTime
 import java.util.*
 
-class WriteDomain(val todoListRepo: TodoListRepo, val todoListEventRepo: TodoListEventRepo, val readDomain: ReadDomain) {
+class WriteDomain(
+    val todoListRepo: TodoListRepo,
+    val todoListEventRepo: TodoListEventRepo,
+    val readDomain: ReadDomain
+) {
     fun createNewTodo(todoName: String): TodoClientView {
-        val newTodo = Todo(id = createID(), createdTimestamp = timeStamp(), lastModifiedTimestamp = timeStamp(), name = todoName)
+        val newTodo =
+            Todo(id = createID(), createdTimestamp = timeStamp(), lastModifiedTimestamp = timeStamp(), name = todoName)
 
         val newTodoEvent = TodoCreatedEvent(
-        eventId = createID(),
-        eventCreatedDate = timeStamp(),
-        entityId = newTodo.id,
-        eventDetails = newTodo
+            eventId = createID(),
+            eventCreatedDate = timeStamp(),
+            entityId = newTodo.id,
+            eventDetails = newTodo
         )
         todoListEventRepo.addEvent(newTodoEvent)
         return todoClientView(newTodo)
     }
+
     fun updateTodoName(todoId: String, updatedTodoName: String): TodoNameUpdate {
 
         val updatedNameTodoEvent = TodoNameUpdatedEvent(
@@ -32,7 +36,6 @@ class WriteDomain(val todoListRepo: TodoListRepo, val todoListEventRepo: TodoLis
         todoListEventRepo.addEvent(updatedNameTodoEvent)
         return updatedNameTodoEvent.eventDetails
     }
-
 
     fun markTodoAsDone(todoId: String): TodoStatusUpdate {
 
@@ -60,35 +63,6 @@ class WriteDomain(val todoListRepo: TodoListRepo, val todoListEventRepo: TodoLis
         return updatedStatusTodoEvent.eventDetails
     }
 
-
-//    fun markTodoAsDone(todoId: String): String {
-//        val todoList: MutableList<Todo> = readDomain.getTodoList("").toMutableList()
-//        var nameOfUpdatedTodo: String? = null
-//        for (todoItem in todoList) {
-//            if (todoItem.id == todoId) {
-//                todoItem.status = "DONE"
-//                todoItem.lastModifiedDate = LocalDateTime.now().toString()
-//                nameOfUpdatedTodo = todoItem.name
-//            }
-//        }
-//        todoListRepo.updateTodoList(todoList)
-//        return "The status of your todo '${nameOfUpdatedTodo}' has been updated to 'DONE'."
-//    }
-
-//    fun markTodoAsNotDone(todoId: String): String {
-//        val todoList: MutableList<Todo> = readDomain.getTodoList("").toMutableList()
-//        var nameOfUpdatedTodo: String? = null
-//        for (todoItem in todoList) {
-//            if (todoItem.id == todoId) {
-//                todoItem.status = "NOT_DONE"
-//                todoItem.lastModifiedDate = LocalDateTime.now().toString()
-//                nameOfUpdatedTodo = todoItem.name
-//            }
-//        }
-//        todoListRepo.updateTodoList(todoList)
-//        return "The status of your todo '${nameOfUpdatedTodo}' has been updated to 'NOT_DONE'."
-//    }
-
     private fun createID(): String {
         return UUID.randomUUID().toString()
     }
@@ -99,14 +73,14 @@ class WriteDomain(val todoListRepo: TodoListRepo, val todoListEventRepo: TodoLis
 
 }
 
-fun main() {
-    val todoListRepo: TodoListRepo = TodoListFileRepo("./src/resources/todo_list.json")
-    val todoListEventRepo: TodoListEventRepo = TodoListEventFileRepo("./src/resources/todo_list_event_log.ndjson")
-    val readDomain = ReadDomain(todoListEventRepo)
-    val writeDomain = WriteDomain(todoListRepo, todoListEventRepo, readDomain)
+//fun main() {
+//    val todoListRepo: TodoListRepo = TodoListFileRepo("./src/resources/todo_list.json")
+//    val todoListEventRepo: TodoListEventRepo = TodoListEventFileRepo("./src/resources/todo_list_event_log.ndjson")
+//    val readDomain = ReadDomain(todoListEventRepo)
+//    val writeDomain = WriteDomain(todoListRepo, todoListEventRepo, readDomain)
 
 //println(writeDomain.markTodoAsDone("c94f495c-6f2e-4f4f-96b2-d2342e59e690"))
 //println(writeDomain.createNewTodo("wash car"))
 //    println(writeDomain.updateTodoName("c94f495c-6f2e-4f4f-96b2-d2342e59e690", "mouse"))
 
-}
+//}
