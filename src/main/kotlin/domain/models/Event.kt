cad68b5e2c7496d3ff9314397750625f012cead2
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-04-15 14:36:56 +0100

    Refactor: EventType made into an enum class

--- src/main/kotlin/domain/models/Event.kt
+++ src/main/kotlin/domain/models/Event.kt
@@ -1,14 +1,14 @@
 package domain.models
 
 sealed class TodoEvent {
-    abstract val eventType: String
+    abstract val eventType: EventType // these abstract properties cannot be instantiated.
     abstract val eventId: String
     abstract val eventCreatedDate: String
     abstract val entityId: String
 }
 
 class TodoCreatedEvent(
-    override val eventType: String = "TODO_CREATED",
+    override val eventType: EventType = EventType.TODO_CREATED,
     override val eventId: String,
     override val eventCreatedDate: String,
     override val entityId: String,
@@ -16,7 +16,7 @@ class TodoCreatedEvent(
 ): TodoEvent()
 
 class TodoNameUpdatedEvent(
-    override val eventType: String = "TODO_NAME_UPDATED",
+    override val eventType: EventType = EventType.TODO_NAME_UPDATED,
     override val eventId: String,
     override val eventCreatedDate: String,
     override val entityId: String,
@@ -24,10 +24,15 @@ class TodoNameUpdatedEvent(
 ): TodoEvent()
 
 class TodoStatusUpdatedEvent(
-    override val eventType: String = "TODO_STATUS_UPDATED",
+    override val eventType: EventType = EventType.TODO_STATUS_UPDATED,
     override val eventId: String,
     override val eventCreatedDate: String,
     override val entityId: String,
     val eventDetails: TodoStatusUpdate
 ): TodoEvent()
 
+enum class EventType {
+    TODO_CREATED,
+    TODO_NAME_UPDATED,
+    TODO_STATUS_UPDATED
+}
