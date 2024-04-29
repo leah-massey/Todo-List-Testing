65af939028ae4b8e33ef794fe5ba59dee142eb98
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-04-29 17:28:06 +0100

    Test: only the id, name and status of items in a todo List is returned

---
+++ src/test/kotlin/domain/ReadDomainTest.kt
@@ -0,0 +1,23 @@
+package domain
+
+import adapters.TodoListEventFileRepo
+import domain.models.Status
+import domain.models.Todo
+import domain.models.TodoClientView
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
+import ports.TodoListEventRepo
+
+class ReadDomainTest {
+    @Test
+    fun `only the id, name and status of items in a todo List is returned`() {
+        val mockEventRepo: TodoListEventRepo = mock(TodoListEventRepo::class.java)
+        `when` (mockEventRepo.getTodoList()).thenReturn(listOf(Todo("123", createdTimestamp = "01/02/24", lastModifiedTimestamp ="01/02/24", name = "wash car", status = Status.NOT_DONE ), Todo("456", createdTimestamp = "02/02/24", lastModifiedTimestamp ="02/02/24", name = "feed cat", status = Status.DONE )))
+        val underTest = ReadDomain(mockEventRepo)
+
+        val expected: List<TodoClientView> = listOf(TodoClientView(id = "123", name = "wash car", status = Status.NOT_DONE), TodoClientView(id = "456", name ="feed cat", status = Status.DONE))
+        val actual: List<TodoClientView> = underTest.getTodoListClientView(null)
+    }
+}
