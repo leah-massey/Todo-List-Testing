c481af12a5b112e864b79675b89896248f490b73
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-05-01 14:22:12 +0100

    Test: an empty todo list is returned when list is empty

--- src/test/kotlin/domain/ReadDomainTest.kt
+++ src/test/kotlin/domain/ReadDomainTest.kt
@@ -10,6 +10,18 @@ import org.mockito.Mockito.`when`
 import ports.TodoListEventRepo
 
 class ReadDomainTest {
+
+    @Test
+    fun `an empty todo list is returned when list is empty`() {
+        val mockEventRepo:TodoListEventRepo = mock(TodoListEventRepo::class.java)
+        `when` (mockEventRepo.getTodoList()).thenReturn(emptyList())
+        val readDomain = ReadDomain(mockEventRepo)
+
+        val expected : List<TodoClientView.FullClientView> = emptyList()
+        val actual: List<TodoClientView.FullClientView> = readDomain.getTodoListClientView(null)
+
+        assertEquals(expected, actual)
+    }
     @Test
     fun `only the id, name and status of items in a todo List is returned`() {
         val mockEventRepo: TodoListEventRepo = mock(TodoListEventRepo::class.java)
@@ -35,7 +47,7 @@ class ReadDomainTest {
     }
 
     @Test
-    fun `only the id and name of todos is returned when filtering by status`() {
+    fun `only the id and name of relevant todos is returned when filtering by NOT DONE status`() {
         val mockEventRepo: TodoListEventRepo = mock(TodoListEventRepo::class.java)
         `when` (mockEventRepo.getTodoList()).thenReturn(listOf(Todo("123", createdTimestamp = "01/02/24", lastModifiedTimestamp ="01/02/24", name = "wash car", status = Status.NOT_DONE )))
         val readDomain = ReadDomain(mockEventRepo)
@@ -45,4 +57,19 @@ class ReadDomainTest {
 
         assertEquals(expected, actual)
     }
+
+    @Test
+    fun `only the id and name of relevant todos is returned when filtering by DONE status`() {
+        val mockEventRepo: TodoListEventRepo = mock(TodoListEventRepo::class.java)
+        `when` (mockEventRepo.getTodoList()).thenReturn(listOf(Todo("123", createdTimestamp = "01/02/24", lastModifiedTimestamp ="01/02/24", name = "wash car", status = Status.NOT_DONE ), Todo("456", createdTimestamp = "02/02/24", lastModifiedTimestamp ="02/02/24", name = "feed cat", status = Status.DONE )))
+        val readDomain = ReadDomain(mockEventRepo)
+
+        val expected: List<TodoClientView.FilteredByStatus> = listOf(TodoClientView.FilteredByStatus(id = "456", name = "feed cat"))
+        val actual: List<TodoClientView.FilteredByStatus> = readDomain.getTodoListByStatusDoneClientView()
+
+        assertEquals(expected, actual)
+    }
+
+    //an empty list - no dones
+    // and empty list
 }
